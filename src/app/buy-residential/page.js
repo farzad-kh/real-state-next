d302414612ca8ce06a53414cf286d029321675b4
farzad kh <farzadkh900@gmail.com>
2023-11-22 17:54:22 +0100

    nb

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -5,24 +5,27 @@ import BuyResidentialsPage from "@/template/BuyResidentialsPage";
 
 async function buyResidential({ searchParams }) {
 
-    // const res = await fetch(`http://localhost:3000/api/profile`, {
-    //     cache: "no-store"
-    // })
+    const res = await fetch(`http://localhost:3000/api/profile`, {
+        cache: "no-store"
+    })
 
-    // const data = await res.json()
-    const profiles = await Profile.find().select("-userId");
-    console.log(profiles);
-    let data = [...profiles]
-    const publish = data?.data?.filter(item => item?.published === true)
-    const filterData = publish?.filter(item => item?.category === searchParams?.category)
+    const data = await res.json()
+    // const profiles = await Profile.find().select("-userId");
 
 
-    // if (data.error) return <h3>مشکلی پیش امده است</h3>
+    const publish = data?.data?.filter(item => item.published === true)
+    const filterData = publish.filter(item => item.category === searchParams?.category)
+
+
+    if (data.error) return <h3>مشکلی پیش امده است</h3>
     return (
         <div>
 
-            <BuyResidentialsPage data={ data}
-            />
+            <BuyResidentialsPage data={
+
+                searchParams.category ? filterData : publish
+
+            } />
         </div>
     );
 };
